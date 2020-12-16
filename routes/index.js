cea96f921afbede826cc43c428e813051c651d4c
Andrei Voinea <andreivdev@protonmail.com>
2020-12-16 21:38:59 +0200

    auth user

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var User = require('../models/user');
 let bcrypt = require('bcrypt');
+let jwt = require('jsonwebtoken');
 
 router.post('/login', function (req, response, next) {
   const { username, password } = req.body;
@@ -13,18 +14,43 @@ router.post('/login', function (req, response, next) {
     (err, user) => {
       {
         if (user) {
-          console.log(user);
           bcrypt.compare(password, user.password, (err, res) => {
             if (res) {
-              response.json('auth succesfull');
+              var token = jwt.sign(
+                { id: user._id },
+                process.env.SESSION_SECRET,
+                {
+                  expiresIn: 3600, // expires in 1 hour
+                }
+              );
+
+              response.json({ auth: 'auth succesfull', token: token });
             } else {
-              response.status(400).json('wrong password');
+              response.status(400).json({ auth: 'wrong password' });
             }
           });
-        } else response.status(400).json('user does not exist');
+        } else response.status(400).json({ auth: 'user does not exist' });
       }
     }
   );
+});
+
+router.get('/protected', function (req, res) {
+  var token = req.headers['x-access-token'];
+  if (!token)
+    return res.status(401).json({ auth: false, message: 'No token provided.' });
+
+  jwt.verify(token, process.env.SESSION_SECRET, function (err, decoded) {
+    if (err)
+      return res
+        .status(500)
+        .json({ auth: false, message: 'Failed to authenticate token.' });
+
+    res.status(200).json({
+      auth: true,
+      message: 'Token valid unitl ' + new Date(decoded.exp * 1000),
+    });
+  });
 });
 
 module.exports = router;
