be8290e866b24576a5573efffa07742d029207f7
Andrei Voinea <andreivdev@protonmail.com>
2020-12-16 15:38:16 +0200

    cleanup

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-router.get('/login', function (req, res, next) {});
 
 module.exports = router;
