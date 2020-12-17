52fd7b6452e162a990c78c90bf57ea5a37ec704b
Andrei Voinea <andreivdev@protonmail.com>
2020-12-17 11:12:24 +0200

    add posts

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var User = require('../models/user');
-let bcrypt = require('bcrypt');
-let jwt = require('jsonwebtoken');
+var User = require("../models/user");
+var Post = require("../models/post");
 
-router.post('/login', function (req, response, next) {
+let bcrypt = require("bcrypt");
+let jwt = require("jsonwebtoken");
+
+router.post("/login", function (req, response, next) {
   const { username, password } = req.body;
   User.findOne(
     {
@@ -26,38 +28,76 @@ router.post('/login', function (req, response, next) {
 
               response.json({
                 auth: true,
-                message: 'auth succesfull',
+                message: "auth succesfull",
                 token: token,
               });
             } else {
               response
                 .status(400)
-                .json({ auth: false, message: 'wrong password' });
+                .json({ auth: false, message: "wrong password" });
             }
           });
         } else
           response
             .status(400)
-            .json({ auth: false, message: 'user does not exist' });
+            .json({ auth: false, message: "user does not exist" });
       }
     }
   );
 });
 
-router.get('/protected', function (req, res) {
-  var token = req.headers['x-access-token'];
+router.get("/protected", function (req, res) {
+  var token = req.headers["x-access-token"];
   if (!token)
-    return res.status(401).json({ auth: false, message: 'No token provided.' });
+    return res.status(401).json({ auth: false, message: "No token provided." });
 
   jwt.verify(token, process.env.SESSION_SECRET, function (err, decoded) {
     if (err)
       return res
         .status(500)
-        .json({ auth: false, message: 'Failed to authenticate token.' });
+        .json({ auth: false, message: "Failed to authenticate token." });
 
     res.status(200).json({
       auth: true,
-      message: 'Token valid unitl ' + new Date(decoded.exp * 1000),
+      message: "Token valid unitl " + new Date(decoded.exp * 1000),
+    });
+  });
+});
+
+router.post("/newpost", function (req, res) {
+  var token = req.headers["x-access-token"];
+  if (!token)
+    return res.status(401).json({ auth: false, message: "No token provided." });
+
+  jwt.verify(token, process.env.SESSION_SECRET, function (err, decoded) {
+    if (err)
+      return res
+        .status(500)
+        .json({ auth: false, message: "Failed to authenticate token." });
+
+    const { username, title, content } = req.body;
+
+    User.findOne({
+      username: username,
+    }).then((user) => {
+      if (!user)
+        return res
+          .status(500)
+          .json({ auth: false, message: "Failed to authenticate token." });
+
+      var newPost = Post({
+        createdBy: user._id,
+        timestamp: new Date(Date.now()),
+        title: title,
+        content: content,
+      });
+
+      newPost
+        .save()
+        .then(() =>
+          res.json({ auth: true, message: "Post " + title + " added." })
+        )
+        .catch((err) => res.status(400).json("Error: " + err));
     });
   });
 });
--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-let User = require('../models/user');
-let bcrypt = require('bcrypt');
+let User = require("../models/user");
+let bcrypt = require("bcrypt");
 
-router.route('/add').post((req, res) => {
+router.route("/add").post((req, res) => {
   const _username = req.body.username;
   const _password = req.body.password;
   const _joinDate = new Date(Date.now());
 
-  User.find({
+  User.findOne({
     username: _username,
   })
-    .limit(1)
     .countDocuments()
     .then((countResult) => {
       if (countResult == 0) {
-        console.log('create user');
+        console.log("create user");
 
         const newUser = new User({
           username: _username,
@@ -30,11 +29,11 @@ router.route('/add').post((req, res) => {
 
         newUser
           .save()
-          .then(() => res.json('User added!'))
-          .catch((err) => res.status(400).json('Error: ' + err));
+          .then(() => res.json("User added!"))
+          .catch((err) => res.status(400).json("Error: " + err));
       } else {
-        console.log('user exists');
-        res.status(400).json('Error: user already exists');
+        console.log("user exists");
+        res.status(400).json("Error: user already exists");
       }
     });
 });
